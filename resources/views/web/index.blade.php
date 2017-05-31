b0749d7817d8a7837e332816c3d96f6dbfeb04a0
wengjunxiao <lwengjunxiao@hotmail.com>
2017-05-31 22:56:27 +0800

    table

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -3,17 +3,9 @@
     <common-header></common-header>
     <body style="margin-top:60px">
       <div class="container">
-          <div style="height: 400px">
-            <div uib-carousel active="active" interval="myInterval">
-              <div uib-slide ng-repeat="slide in slides track by slide.id" index="slide.id" style="height:400px;">
-                <img ng-src="@{{slide.image}}" style="width: 100%; margin:auto;">
-                <div class="carousel-caption">
-                  <h4>Slide @{{slide.id}}</h4>
-                  <p>@{{slide.text}}</p>
-                </div>
-              </div>
-            </div>
-          </div>
+              @foreach ($items as $item)
+            <li>{{$item}}   </li>
+         @endforeach
         <common-footer></common-footer>
        </div>
     </body>
